cc0a84aa6dd91bcf8c9187ecf75a283aea17b507
feminaschittalakkott <feminasc442@gmail.com>
2024-09-16 11:54:39 +0400

    Initial

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import json-server
+const jsonserver = require('json-server');
+
+// creating server
+const mpServer = jsonserver.create();
+
+// Initializing middleware
+const middleware = jsonserver.defaults();
+
+// creating router instance for resources
+const router = jsonserver.router('db.json');
+
+// configuring middleware and router to server
+mpServer.use(middleware);
+mpServer.use(router);
+
+// setting port number
+const PORT = 3000 || process.env.PORT;
+
+// calling listen() method with server for activating server
+mpServer.listen(PORT, ()=>{
+    console.log(`Server Running at PORT ${PORT}`)
+})
